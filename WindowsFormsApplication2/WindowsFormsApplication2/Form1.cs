c01945483b64719287da3a0ddfaa6d4e73be7b24
Jaws <christian_markowicz@hotmail.com>
2014-01-20 15:53:05 +0100

    test
    test

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Func1()
+        {
+            //MYSTERISK SKRIV HÄR
+        }
+        private void Func2()
+        {
+            //JAWS SKRIV HÄR
+        }
+    }
+}
